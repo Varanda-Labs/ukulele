a40ebd061da4b9c92e4169fdd5e24a70d7022a7f
Marcelo Varanda <Marcelo.Varanda@sbdinc.com>
2023-10-12 19:18:40 -0400

    add sound

--- Ukulele-Godot/AnimateStrings.gd
+++ Ukulele-Godot/AnimateStrings.gd
@@ -18,22 +18,24 @@ var _notes_factors = [
 	1.6817486231210919,
 	1.7817453962518721,
 	1.8876879775130087,
+	2.0,
 ]
 
-var _notes_names = [
-	"A",
-	"A#",
-	"B",
-	"C",
-	"C#",
-	"D",
-	"D#",
-	"E",
-	"F",
-	"F#",
-	"G",
-	"G#"
-]
+var _notes_names = {
+	"A": 0,
+	"A#": 1,
+	"B": 2,
+	"C": 3,
+	"C#":4,
+	"D":5,
+	"D#":6,
+	"E":7,
+	"F":8,
+	"F#":9,
+	"G":10,
+	"G#":11,
+	"A+": 12,
+}
 
 	
 
@@ -42,14 +44,19 @@ var _notes_names = [
 func _on_play(v, _stop = false):
 	print("_on_play: " + str(v))
 	var s
+	var note = ""
 	if v == 1:
 		s = "String-1"
+		note = "A"
 	elif v == 2:
 		s = "String-2"
+		note = "B"
 	elif v == 3:
 		s = "String-3"
+		note = "C#"
 	elif v == 4:
 		s = "String-4"
+		note = "D"
 	else:
 		print("wrong node")
 		return
@@ -57,6 +64,7 @@ func _on_play(v, _stop = false):
 		stop()
 	else:
 		play(s)
+		play_note(note)
 	
 
 # Called when the node enters the scene tree for the first time.
@@ -70,7 +78,15 @@ func _ready():
 	elif self.name == "AnimationPlayer_4":
 		ukulele_node.play_string_4_requested.connect(_on_play)
 
-
+func play_note(n):
+	var audio := AudioStreamPlayer.new()
+	add_child(audio)
+	audio.stream = preload("res://tunes/ukulele_la.440.wav")
+	audio.pitch_scale = _notes_factors[_notes_names[n]]
+	audio.play()
+	#color_timer.start()
+	await get_tree().create_timer(8.0).timeout
+	audio.queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
