973d5fc91db1b96d9fee8657721431b114a70f75
Marcelo Varanda <Marcelo.Varanda@sbdinc.com>
2023-10-11 20:10:04 -0400

    adding some math stuff for music notes.

--- Ukulele-Godot/AnimateStrings.gd
+++ Ukulele-Godot/AnimateStrings.gd
@@ -3,6 +3,42 @@ extends AnimationPlayer
 @onready var game_root_node = $/root/Node3D
 @onready var ukulele_node = $/root/Node3D/Ukulele
 
+var _NOTE_FACTOR = 1.05946
+
+var _notes_factors = [
+	1.0,
+	1.05946,
+	1.1224554916000002,
+	1.1891966951305362,
+	1.259906330622998,
+	1.3348203610418417,
+	1.4141887797093895,
+	1.49827644455091,
+	1.587363961943907,
+	1.6817486231210919,
+	1.7817453962518721,
+	1.8876879775130087,
+]
+
+var _notes_names = [
+	"A",
+	"A#",
+	"B",
+	"C",
+	"C#",
+	"D",
+	"D#",
+	"E",
+	"F",
+	"F#",
+	"G",
+	"G#"
+]
+
+	
+
+
+
 func _on_play(v, _stop = false):
 	print("_on_play: " + str(v))
 	var s
